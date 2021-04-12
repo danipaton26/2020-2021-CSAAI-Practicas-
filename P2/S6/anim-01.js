105a20e39d5aecc7b7b0a0e078f348fd9bf013c0
danipaton26 <dani2611paton@gmail.com>
2021-04-12 13:18:10 +0200

    animación completa

--- P2/S6/anim-01.js
+++ P2/S6/anim-01.js
@@ -5,16 +5,24 @@ canvas.height = 100;
 
 const ctx = canvas.getContext("2d");
 
-let x = 0;
-let y = 0;
+let x = 1;
+let y = 1;
 
-let velx = 1;
+let velx = 3;
+let vely = 0.5;
 
 function update()
     {
         console.log("test");
-        x = x + velx;
+        if (x <=0 || x >= canvas.width - 20){
+          velx = -velx
+        }
+        if (y <=0 || y>= canvas.height - 20){
+          vely = -vely
+        }
 
+        x = x + velx;
+        y = y + vely;
        ctx.clearRect(0, 0, canvas.width, canvas.height);
        
        ctx.beginPath();
