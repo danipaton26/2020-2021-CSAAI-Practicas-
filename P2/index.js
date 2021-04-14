f7a50ba8d1d8461a72216ad049ec33fda9ae6563
danipaton26 <dani2611paton@gmail.com>
2021-04-14 14:10:40 +0200

    introduzco principio de js

---
+++ P2/index.js
@@ -0,0 +1,8 @@
+console.log("Ejecutando js...")
+
+// Elementos de la calculadora
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+resta = document.getElementById("resta")
+multiplicacion = document.getElementById("multiplicacion")
+division = document.getElementById("division")
