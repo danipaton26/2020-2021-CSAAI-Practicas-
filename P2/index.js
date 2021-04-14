ac96933b79616dc790576cb1c233450cc9261018
danipaton26 <dani2611paton@gmail.com>
2021-04-14 14:24:46 +0200

    lee el primer numero

--- P2/index.js
+++ P2/index.js
@@ -2,7 +2,17 @@ console.log("Ejecutando js...")
 
 // Elementos de la calculadora
 display = document.getElementById("display")
-suma = document.getElementById("suma")
-resta = document.getElementById("resta")
-multiplicacion = document.getElementById("multiplicacion")
-division = document.getElementById("division")
+borrar = document.getElementById("borrar")
+clear = document.getElementById("clear")
+igual = document.getElementById("igual")
+
+//Arrays de los dígitos y de los operadores
+digito = document.getElementsByClassName("digito")
+operador = document.getElementsByClassName("operador")
+
+for (let numero of digito){
+    numero.onclick = (ev) => {
+        display.innerHTML += ev.target.value
+        console.log("digito!!")
+    }
+}
