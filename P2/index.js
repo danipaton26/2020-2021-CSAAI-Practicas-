450d00467a7b4e3d2903d8a80b9a5921489e145e
danipaton26 <dani2611paton@gmail.com>
2021-04-22 16:37:37 +0200

    No me va

--- P2/index.js
+++ P2/index.js
@@ -7,12 +7,55 @@ clear = document.getElementById("clear")
 igual = document.getElementById("igual")
 
 //Arrays de los dígitos y de los operadores
-digito = document.getElementsByClassName("digito")
-operador = document.getElementsByClassName("operador")
+let digito = document.getElementsByClassName("digito");
+let operador = document.getElementsByClassName("operador");
+
+//Estados de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3,
+}
+let estado = ESTADO.INIT;
+
+function digito(ev){
+
+    if (estado == ESTADO.INIT) {
+        display.innerHTML = ev.target.value;
+        estado = ESTADO.OP1;
+    }
+    else{
+        display.innerHTML += ev.target.value;
+    }
+}
 
 for (let numero of digito){
     numero.onclick = (ev) => {
         display.innerHTML += ev.target.value
-        console.log("digito!!")
+        console.log("digito")
     }
+}
+
+for (let operacion of operador) {
+    operacion.onclick = (ev) => {
+        if (estado == ESTADO.OP1){
+            display.innerHTML += ev.target.value;
+            estado = ESTADO.OPERATION;
+            console.log("operador") 
+        }
+       
+    }
+}
+
+igual.onclick = () => {
+    if (estado == ESTADO.OP1 || estado == ESTADO.OP2) {
+        display.innerHTML = eval(display.innerHTML);
+        estado = ESTADO.OP1;
+    }
+}
+
+clear.onclick = (ev) => {
+    display.innerHTML = 0;
+    console.log("clear")
 }
