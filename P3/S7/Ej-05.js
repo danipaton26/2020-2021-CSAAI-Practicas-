f1942232cf012a4f3c56c2261e69c8936000a15e
danipaton26 <dani2611paton@gmail.com>
2021-04-28 19:07:17 +0200

    Ej 5 s7

---
+++ P3/S7/Ej-05.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("display")
+
+//-- Tecla pulsada: Mostrar su información
+window.onkeydown = (e)=> {
+    display.innerHTML = `Tecla: ${e.key}. Código: ${e.keyCode}`
+  }
+  
+  //-- Tecla liberada: Borrar el párrafo
+  window.onkeyup = (e) => {
+    display.innerHTML = ""
+  }
