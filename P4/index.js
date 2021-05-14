e6da9df0077ce12bc14ccf0bd7e46970d15b2416
danipaton26 <dani2611paton@gmail.com>
2021-05-14 17:22:26 +0200

    van todos los colores

--- P4/index.js
+++ P4/index.js
@@ -6,9 +6,13 @@ const ctx = canvas.getContext('2d');
 
 //-- Acceso al deslizador
 const deslizador = document.getElementById('deslizador');
+const deslizadorg = document.getElementById('deslizadorg');
+const deslizadorb = document.getElementById('deslizadorb');
 
 //-- Valor del deslizador
 const range_value = document.getElementById('range_value');
+const range_valueg = document.getElementById('range_valueg');
+const range_valueb = document.getElementById('range_valueb');
 
 img.onload = function () {
     console.log("imagen cargada");
@@ -23,10 +27,18 @@ img.onload = function () {
 
 deslizador.oninput = () => {
     //-- Mostrar el nuevo valor del deslizador
-  rojo();
+  colores();
+}
+deslizadorg.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+  colores();
+}
+deslizadorb.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+  colores();
 }
 
-function rojo() {
+function colores() {
     canvas.width = img.width;
     canvas.height = img.height;
 
@@ -38,12 +50,30 @@ function rojo() {
     let data = imgData.data;
     range_value.innerHTML = deslizador.value;
     valorrojo = deslizador.value
+
+    range_valueg.innerHTML = deslizadorg.value;
+    valorverde = deslizadorg.value
+
+    range_valueb.innerHTML = deslizadorb.value;
+    valorazul = deslizadorb.value
+
     for (let i = 0; i < data.length; i+=4) {
         if (data[i] > valorrojo) 
             data[i] = valorrojo;
+        if (data[i + 1] > valorverde) 
+            data[i + 1] = valorverde;
+        if (data[i + 2] > valorazul) 
+            data[i + 2] = valorazul;
     }
 
     ctx.putImageData(imgData, 0, 0);
 }
+
+
+
+
+
+
+
 console.log("fin")
 
