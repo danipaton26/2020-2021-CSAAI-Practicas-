8022c0223558e1f8103d0984a90f09a5c80e00ba
danipaton26 <dani2611paton@gmail.com>
2021-05-14 15:33:17 +0200

    umbral de rojo

--- P4/index.js
+++ P4/index.js
@@ -4,6 +4,12 @@ const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc')
 const ctx = canvas.getContext('2d');
 
+//-- Acceso al deslizador
+const deslizador = document.getElementById('deslizador');
+
+//-- Valor del deslizador
+const range_value = document.getElementById('range_value');
+
 img.onload = function () {
     console.log("imagen cargada");
 
@@ -11,25 +17,32 @@ img.onload = function () {
     canvas.height = img.height;
     ctx.drawImage(img, 0,0);
 
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    let data = imgData.data
+};
 
-    for (let i = 0; i < data.length; i+=4) {
-        data[i] = 0; //-- Canal rojo a 0
-      }
+deslizador.oninput = () => {
+    //-- Mostrar el nuevo valor del deslizador
+  range_value.innerHTML = deslizador.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Obtener el umbral de rojo del desliador
+  umbral = deslizador.value
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    if (data[i] > umbral)
+      data[i] = umbral;
+  }
 
   //-- Poner la imagen modificada en el canvas
   ctx.putImageData(imgData, 0, 0);
-
-    console.log("Tamañp de data: " + data.length)
-
-    npixels = canvas.width * canvas.height
-  console.log("Anchura (en pixeles): " + canvas.width)
-  console.log("Altura (en pixeles): " + canvas.height)
-  console.log("Pixeles totales: " + npixels)
-
-  //-- Puesto que cada pixel ocupa 4 bytes, el array de píxeles
-  //-- tiene un tamaño de 4 * numero de pixeles
-  console.log("Total de datos de la imagen: " + npixels * 4)
-};
+}
 console.log("fin")
